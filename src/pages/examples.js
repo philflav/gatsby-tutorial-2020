a3ff4f68ab01a782a1675a69d644c8ae0fc2fd47
Phil <pflavin@gmail.com>
2020-08-15 17:25:27 +0100

    header removed

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import Layout from '../components/layout';
-import Header from '../examples/Header';
 
 const examples = () => {
   return (
     <Layout>
       <div>
         <h1>Examples Page</h1>
-        <Header />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam voluptates assumenda alias magni voluptatibus eveniet a, molestiae ratione omnis doloremque illo eum impedit quas cupiditate quo debitis, quibusdam accusantium adipisci fuga sequi itaque odit mollitia? Vero aliquam expedita, repellat illo inventore iusto accusantium laboriosam totam autem, assumenda recusandae tempore facilis cum culpa impedit? Qui, suscipit, ut labore, nobis dolorum nihil ea reiciendis voluptate possimus sunt at numquam mollitia. Eum laboriosam quasi fugit veniam iusto tenetur tempore ex cumque! Vitae ad eius velit aliquam ut dicta maxime eveniet, commodi totam molestias tempora pariatur! Quos dolore esse reiciendis odit molestiae voluptate.
+        </p>
       </div>
     </Layout>
   );
