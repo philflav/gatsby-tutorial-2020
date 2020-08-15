c5c692c05f2e6252dfe65d06068e9526c6e9fa6d
Phil <pflavin@gmail.com>
2020-08-15 17:34:57 +0100

    about message updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,9 @@ export default function About () {
   return (
     <Layout>
       <h2>About Page</h2>
+      <p>
+        This is just a tutorial application to learn about Gatsby, GraphQL and continuous deployment to Netlify
+      </p>
     </Layout>
   );
 }
